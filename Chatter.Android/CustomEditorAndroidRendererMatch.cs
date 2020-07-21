669e6c4a7c277715a6f025224753993c502965da
chrisdelier <christineedoo@yahoo.com>
2020-07-22 01:37:31 +0900

    modified match page, new edit profile&preferences icons, no loc in inbox, resized reportuser button

---
+++ Chatter.Android/CustomEditorAndroidRendererMatch.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Chatter.Droid;
+using Xamarin.Forms;
+using Chatter.Classes;
+using Xamarin.Forms.Platform.Android;
+using Android.Graphics.Drawables;
+
+[assembly: ExportRenderer(typeof(CustomEditorMatch), typeof(CustomEditorAndroidRendererMatch))]
+
+namespace Chatter.Droid
+{
+    public class CustomEditorAndroidRendererMatch : EditorRenderer
+    {
+        public CustomEditorAndroidRendererMatch(Context context) : base(context)
+        {
+
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<Editor> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                GradientDrawable gd = new GradientDrawable();
+                gd.SetCornerRadius(30f);
+                gd.SetStroke(10, Android.Graphics.Color.Transparent);
+                Control.SetAutoSizeTextTypeWithDefaults(AutoSizeTextType.Uniform);
+                Control.SetMinHeight(200);
+                Control.SetBackground(gd);
+
+                Control.SetPadding(30, 20, 30, 20);
+            }
+        }
+    }
+}
--- Chatter/View/AnimateMatched.xaml.cs
+++ Chatter/View/AnimateMatched.xaml.cs
@@ -12,7 +12,7 @@ using Xamarin.Forms.Xaml;
 namespace Chatter.View
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class AnimateMatched : PopupPage
+    public partial class AnimateMatched : ContentPage
     {
         public string ImageOne, ImageTwo;
         public AnimateMatched(string userImage,string likedUserImage)
@@ -21,27 +21,27 @@ namespace Chatter.View
             ImageOne = userImage;
             ImageTwo = likedUserImage;
         }
-        protected async override void OnAppearing()
+        protected override void OnAppearing()
         {
             yourImage.Source = ImageOne;
             othersImage.Source = ImageTwo;
-            await Task.Delay(1000);
-            yourImage.RotateTo(360,1500);
-            othersImage.RotateTo(-360, 1500);
-            yourImage.TranslateTo(100,0, 1500);
-            othersImage.TranslateTo(-100, 0, 1500);
-            yourImage.ScaleTo(0, 1500);
-            othersImage.ScaleTo(0, 1500);
-            heartImage.IsVisible = true;
-            heartImage.FadeTo(1, 2000);
-            await heartImage.ScaleTo(0,100);
-            await heartImage.ScaleTo(1,2000);
+            // await Task.Delay(1000);
+            //  yourImage.RotateTo(360,1500);
+            // othersImage.RotateTo(-360, 1500);
+            // yourImage.TranslateTo(100,0, 1500);
+            //  othersImage.TranslateTo(-100, 0, 1500);
+            //  yourImage.ScaleTo(0, 1500);
+            //  othersImage.ScaleTo(0, 1500);
+            //  heartImage.IsVisible = true;
+            //  heartImage.FadeTo(1, 2000);
+            //  await heartImage.ScaleTo(0,100);
+            //  await heartImage.ScaleTo(1,2000);
 
         }
 
-        private async void Button_Clicked_1(object sender, EventArgs e)
+        private void Button_Clicked_1(object sender, EventArgs e)
         {
-            await PopupNavigation.Instance.PopAsync();
+            Navigation.PopModalAsync();
         }
 
     }
